f3b9c492fb92a6104a0ae812e6151bfd64555bd5
Gabriel_O <55938114+gabrielolafs@users.noreply.github.com>
2026-02-15 00:05:53 -0500

    feat: circle buttons for main page are looking pretty darn good

--- main.tsx
+++
@@ -1,6 +0,0 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { createRoot } from 'react-dom/client';
-import PageFlip from './PageFlip';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<PageFlip />)
