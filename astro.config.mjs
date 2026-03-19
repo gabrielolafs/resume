598dd643b08f0fea761922e00837d5d544016c31
Gabriel_O <55938114+gabrielolafs@users.noreply.github.com>
2026-03-19 14:46:14 -0400

    chore: docs for blender / main page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+// export default defineConfig({});
+
+export default defineConfig({
+    server: {
+        host: false, // true if you are at home and want to test mobile
+        port: 4321,
+    }
+});
