c893bcd64f50f554100bbc97fdca82db253c91c6
Gabriel_O <55938114+gabrielolafs@users.noreply.github.com>
2026-03-20 16:14:59 -0400

    feat: salt so not every animation looks the same

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     server: {
-        host: false, // true if you are at home and want to test mobile
+        host: true, // true if you are at home and want to test mobile
         port: 4321,
     }
 });
